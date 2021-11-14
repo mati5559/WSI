6751993de550a7711aa8fc97776250769672eca2
Mateusz Dąbrowski <mati5559@interia.pl>
2021-11-14 17:35:27 +0100

    Improved generating random graph, added some new arguments.

--- 2_graph_coloring/graph.py
+++ 2_graph_coloring/graph.py
@@ -2,31 +2,34 @@ import random
 import json
 
 class Graph:
-    def __init__(self, nodes=[]):
-        if(type(nodes) is str):
-            nodes = json.loads(nodes)
-        self.nodes = nodes
+    def fromJSON(self, input):
+        input = json.loads(input)
+        self.nodes = input.nodes
+        self.colors = input.colors
+
+
+    def toJSON(self):
+        return json.dumps({"nodes": self.nodes, "colors": self.colors})
 
 
     def generateRandom(self, nodesNumber, fillFactor):
         self.nodes = []
-        for i in range(0, nodesNumber):
-            connections = []
-
-            for x in range(0, nodesNumber):
-                if((random.random()<fillFactor) & (x!=i)):
-                    connections.append(x)
-
+        self.colors = []
+        
+        # Generatring nodes and colors
+        for _ in range(0, nodesNumber):
             self.nodes.append({
                 "x": random.uniform(0, 1000),
                 "y": random.uniform(0, 1000),
-                "color": 1,
-                "connections": connections
+                "connections": []
             })
+            self.colors.append(int(random.uniform(0, nodesNumber)))
 
-
-    def toJSON(self):
-        return json.dumps(self.nodes)
-
-
+        # Generating connections between nodes
+        for index, node in enumerate(self.nodes):
+            for i in range(0, nodesNumber):
+                if(i!=index):
+                    if((i not in node["connections"]) & (random.random()<fillFactor)):
+                        node["connections"].append(i)
+                        self.nodes[i]["connections"].append(index)
     
