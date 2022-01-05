82f3e7d4ef6dffb27ef627860047ece5fb0d8f6a
Mateusz Dąbrowski <mati5559@interia.pl>
2022-01-05 19:50:52 +0100

    Implemented some operations on files

--- 5_neural_network/neural_network.py
+++ 5_neural_network/neural_network.py
@@ -1,6 +1,8 @@
 import random
 import math
 import decimal
+import json
+
 def sigmaFunction(x):
     return int(1/(1+(decimal.Decimal(math.e)**(decimal.Decimal(-x)))))
 
@@ -33,14 +35,18 @@ class Neuron:
 
         
 class NeuralNetwork:
-    def __init__(self, layerWidth, layersNumber, inputsNumber):
+    def __init__(self, layerWidth=None, layersNumber=None, inputsNumber=None, filename=None):
+        if filename is not None:
+            self.loadFromFile(filename)
+            return
+
         self.layers = []
         self.layers.append([])
 
         for _ in range(0, layerWidth):
             self.layers[0].append(Neuron(sigmaFunction, None, inputsNumber))
         
-        for _ in range(0, layersNumber):
+        for _ in range(1, layersNumber):
             layer = []
             
             for _ in range(0, layerWidth):
@@ -50,7 +56,7 @@ class NeuralNetwork:
 
         self.outputNode = Neuron(linearFunction, self.layers[-1])
 
-    def getResult(self, input):
+    def getResult(self, input) -> int:
         previousLayerResult = input
         for layer in self.layers:
             nextResults = []
@@ -61,3 +67,47 @@ class NeuralNetwork:
         
         return self.outputNode.getResult(previousLayerResult)
     
+    def saveToFile(self, fileName):
+        weights = []
+        for layer in self.layers:
+            weightsLayer = []
+            for neuron in layer:
+                weightsLayer.append(neuron.weights)
+            weights.append(weightsLayer)
+
+        weights.append([self.outputNode.weights])
+        
+        with open(fileName, 'w') as file:
+            json.dump({
+                "weights":weights
+            }, file)
+
+    def loadFromFile(self, filename):
+        weights = None
+        with open(filename, 'r') as file:
+            weights = json.load(file)['weights']
+
+        self.layers = []
+        self.layers.append([])
+
+
+        firstLayer = weights.pop(0)
+        for neuronWeights in firstLayer:
+            newNeuron = Neuron(sigmaFunction, None, len(neuronWeights))
+            newNeuron.weights = neuronWeights
+            self.layers[0].append(newNeuron)
+        
+        for _ in range(0, len(weights)-1):
+            layer = weights.pop(0)
+            newLayer = []
+            for neuronWeights in layer:
+                newNeuron = Neuron(sigmaFunction, self.layers[-1])
+                newNeuron.weights = neuronWeights
+                newLayer.append(newNeuron)
+
+            self.layers.append(newLayer)
+
+        self.outputNode = Neuron(linearFunction, self.layers[-1])
+        self.outputNode.weights = weights[-1][0]
+            
+        
