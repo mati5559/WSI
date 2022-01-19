287e63ba798fab93ff9ca8b8be882a2ec3b4cee7
Mateusz Dąbrowski <mati5559@interia.pl>
2022-01-19 20:03:54 +0100

    Implemented drivers and O-Learning algorithm

--- 6_q_uber/board.py
+++ 6_q_uber/board.py
@@ -3,38 +3,51 @@ import copy
 
 class Board:
     def __init__(self, n, start=None, end=None):
-        self.fields = [[" " for _ in range(0, n+2)] for _ in range(0, n+2)]
+        self.fields = [[" " for _ in range(0, n+2)] for _ in range(0, n+2)] 
 
         if start is None:
             start = [int(random.uniform(1, n)), int(random.uniform(1, n))]
         if end is None:
             end = [int(random.uniform(1, n)), int(random.uniform(1, n))]
 
+        # Expand board to add borders
+        start[0] += 1
+        start[1] += 1
+        end[0] += 1
+        end[1] += 1
+
         self.randomize(start, end)
 
         while(not self.check(start)):
             self.randomize(start, end)
 
+        self.playerPosition = start
+        self.status = "running"
 
     def randomize(self, start, end):
         for row in self.fields:
+            # Left and right borders
             row[0] = "\u2588"
             row[-1] = "\u2588"
+            # Random space
             for i in range(1, len(row)-1):
                 row[i] = random.choice([" ", " ", " ", " ", "\u2588", "\u2588"]) # Generate more free space than obstacles
 
+        # Top and bottom borders
         self.fields[0] = ["\u2588" for _ in range(0, len(self.fields[0]))]
         self.fields[-1] = ["\u2588" for _ in range(0, len(self.fields[-1]))]
 
-        self.fields[start[1]+1][start[0]+1] = "S"
-        self.fields[end[1]+1][end[0]+1] = "E"
+        # Start and end
+        self.fields[start[1]][start[0]] = "S"
+        self.fields[end[1]][end[0]] = "E"
 
     def check(self, startPosittion) -> bool:
         # Check if there is any possible route between start and end
         fieldsCopy = copy.deepcopy(self.fields)
 
+        # DFS algorithm
         def visitField(field):
-            fieldsCopy[field[1]+1][field[0]+1] = "X"
+            fieldsCopy[field[1]][field[0]] = "X"
 
             neighbours = [
                 [field[0], field[1]+1],
@@ -44,9 +57,9 @@ class Board:
             ]
 
             for neighbour in neighbours:
-                if(fieldsCopy[neighbour[1]+1][neighbour[0]+1] == "E"):
+                if(fieldsCopy[neighbour[1]][neighbour[0]] == "E"):
                     return True
-                elif(fieldsCopy[neighbour[1]+1][neighbour[0]+1] == " "):
+                elif(fieldsCopy[neighbour[1]][neighbour[0]] == " "):
                     result = visitField(neighbour)
                     if(result):
                         return True
@@ -57,11 +70,40 @@ class Board:
     def print(self):
         print("\u2588 - obstacles")
         print("  - free fields")
+        print("X - visited fields")
+        print("P - player position")
         print("S - start")
         print("E - end")
+        
         print("")
 
         for rowIndex in range(1, len(self.fields)+1):
             for field in self.fields[-rowIndex]:
                 print(field, end="")
             print("")
+
+    # Returns False when player hit obstacle
+    def move(self, direction) -> bool:
+        if(self.fields[self.playerPosition[1]][self.playerPosition[0]] not in ["S", "E"]):
+            self.fields[self.playerPosition[1]][self.playerPosition[0]] = "X"
+
+        if(direction == 0): # left
+            self.playerPosition[0] -= 1
+        if(direction == 1): # up
+            self.playerPosition[1] += 1
+        if(direction == 2): # right
+            self.playerPosition[0] += 1
+        if(direction == 3): # down
+            self.playerPosition[1] -= 1
+        
+        
+       
+        if(self.fields[self.playerPosition[1]][self.playerPosition[0]] != "\u2588"):
+            if(self.fields[self.playerPosition[1]][self.playerPosition[0]] == "E"):
+                self.status = "win"
+            self.fields[self.playerPosition[1]][self.playerPosition[0]] = "P"
+            
+            return True
+        else:
+            self.status = "lost"
+            return False
--- 6_q_uber/main.py
+++ 6_q_uber/main.py
@@ -1,10 +1,11 @@
 import argparse
 import board
 import random
+import drivers
 
 # Configure parser and get parsed arguments
 def getArguments():
-    parser = argparse.ArgumentParser(description='Q-Uber')
+    parser = argparse.ArgumentParser(description='Finding best route from start to end with O-Learning algorithm - \"Q-Uber\"')
 
     parser.add_argument('N', type=int, help='Board size')
 
@@ -13,7 +14,18 @@ def getArguments():
 
     parser.add_argument('--seed', type=int, help='Seed for random generator')
 
-    parser.add_argument('--print', action="store_true", help='Print board in console')
+    rqGroup = parser.add_mutually_exclusive_group(required=True)
+    rqGroup.add_argument('--random', action="store_true", help='Use random driver')
+    rqGroup.add_argument('--quber', action="store_true", help='Use driver, that uses Q-learning algorithm')
+
+    parser.add_argument('--print', action="store_true", help='Print board with example run in console')
+
+    parser.add_argument('--epoch', type=int, default=500, help="Number of epochs for QUber (default 500)")
+    parser.add_argument('--beta', type=float, default=1, help="Beta parameter for QUber (default 1)")
+    parser.add_argument('--df', type=float, default=1, help="Discount factor for QUber (default 1)")
+
+
+    parser.add_argument('--test', type=int, metavar="X", help='Test driver X times')
 
     return parser.parse_args()
 
@@ -28,10 +40,28 @@ def main():
         print("Seed:", seed)
 
     brd = board.Board(args.N, args.start, args.end)
+    driver = None
+    if(args.random):
+        driver = drivers.RandomDriver(brd)
+    else:
+        driver = drivers.QUber(brd)
+        driver.train(args.beta, args.df, args.epoch)
+
+    brd = driver.start()
+    driver.clearBoard()
 
     if(args.print):
         brd.print()
 
+    if args.test is not None:
+        successes = 0
+        for _ in range(0, args.test):
+            result = driver.start()
+            driver.clearBoard()
+            if(result.status == "win"):
+                successes += 1
+        print("Driver reached goal in " + str(successes) + " per " + str(args.test) + " times.")
+        
 
 if __name__ == "__main__":
     main()
